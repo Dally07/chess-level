c7470f278aaee34269cd9844d78a8dca9cc00f89
Dally <dallymaminomenjanahar@gmail.com>
2024-10-31 19:14:14 +0300

    table d'echec

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import api from './api';
+import React from 'react';
 import Chessboard from './Chessboard';
 
 const App: React.FC = () => {
-    const [message, setMessage] = useState<string>('');
-
-    useEffect(() => {
-        api.get('/')
-            .then(response => setMessage(response.data))
-            .catch(error => console.error("Erreur d'API", error));
-    }, []);
 
     return (
         <div>
-            <h1>Jeu d'Échecs</h1>
-            <p>Message du backend : {message}</p>
+          <h1 className='text-xs'>chess</h1>
             <Chessboard />
         </div>
     );
--- frontend/src/Chessboard.tsx
+++ frontend/src/Chessboard.tsx
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ChessGame } from './chessGame';
+
+const pieceSymbols: { [key: string]: string } = {
+    'p': '♙', 'r': '♖', 'n': '♘', 'b': '♗', 'q': '♕', 'k': '♔',  // Pièces blanches
+    'P': '♟', 'R': '♜', 'N': '♞', 'B': '♝', 'Q': '♛', 'K': '♚'   // Pièces noires
+};
 
 const Chessboard: React.FC = () => {
     const game = new ChessGame();
+    const [board] = useState(game.getBoard());
 
     const renderBoard = () => {
-        const board = game.getBoard();
         return board.map((row, rowIndex) => (
             <div key={rowIndex} className="flex">
-                {row.map((piece, colIndex) => (
-                    <div key={colIndex} className="w-16 h-16 flex items-center justify-center">
-                        {piece ? piece.type : null}
+                {row.map((square, colIndex) => (
+                    <div key={colIndex} className="w-16 h-16 flex items-center justify-center text-red-500 border">
+                        {square ? pieceSymbols[square.color === 'w' ? square.type.toUpperCase() : square.type] : ''}
                     </div>
                 ))}
             </div>
         ));
     };
 
-    return <div className="chessboard">{renderBoard()}</div>;
+    return (
+        <div className="chessboard">
+            {renderBoard()}
+        </div>
+    );
 };
 
 export default Chessboard;
